bd840c369983e79981f4b69320316a5739771547
Ihar Voitau <Ihar_Voitau@epam.com>
2017-05-17 18:21:33 +0300

    adding basic AI example

--- BombermanCustomAI.ps1
+++ BombermanCustomAI.ps1
@@ -1,97 +1,130 @@
-﻿#region Quickstart
-
+﻿# QuickStart
 Import-Module .\BombermanAPI.psm1 -Force
-
 [URI]$Global:BombermanURI = "ws://127.0.0.1:8080/codenjoy-contest/ws?user=username@users.org"
+while ($true) {Invoke-GameSync}
+
+
+# Basics
+
+# to make a move use Invoke-GameSync function's -NextAction parameter
+Invoke-GameSync -NextAction 'act, down'
+# or pipe your action to Invoke-GameSync
+'act, up' | Invoke-GameSync
+# or use 'move' alias
+move up
+# to do nothing and to receive gameboard situation 
+Invoke-GameSync -NextAction wait
  
-while ($true) 
-{
-	
-	#Clear-Host
-	#Measure-Command {Invoke-GameSync}
-	Invoke-GameSync | Show-GameBoardRawGrid
-	
-}
 
-#endregion
-
-#region 
-
-
-
+# make console GUI
 while ($true)
 {
-	
-	$myBoardString = Get-GameBoardRawString 
-	$myCurrentGameBoard = Get-GameBoardElementArray -GameBoardRawString $myBoardString
-
-	$myBomber = Get-GameElementCollection -GameBoardRawString $myBoardString -Element Bomberman
-
-	# X,Y of myBomber
-	$x = $myBomber[0][0]
-	$y = $myBomber[0][1]
-
-
-	# Place bomb
-
-	Measure-Command {Invoke-GameAction-rev205 -BombermanAction act}
-
-	Invoke-GameAction-rev205 -BombermanAction act
-
-	# Get out of boom aray
-	if ($myCurrentGameBoard[($x+1),($y)] -notmatch "Wall","WallDestroyable")
-	{
-		Invoke-GameAction-rev205 -BombermanAction right
-	}
-	if ($myCurrentGameBoard[($x-1),($y)] -notmatch "Wall","WallDestroyable")
-	{
-		Invoke-GameAction-rev205 -BombermanAction left
-	}
-	if ($myCurrentGameBoard[($x),($y+1)] -notmatch "Wall","WallDestroyable")
-	{
-		Invoke-GameAction-rev205 -BombermanAction up
-	}
-	if ($myCurrentGameBoard[($x),($y-1)] -notmatch "Wall","WallDestroyable")
-	{
-		Invoke-GameAction-rev205 -BombermanAction down
-	}
-
-
-
-}
-
-
-while ($true) {
-	Invoke-GameAction -BombermanAction left
-}
-
-
-
-# realtime consile GUI
-while ($true) {
-	Get-GameBoardRawString | Show-GameBoardCharArray
 	Clear-Host
+	Invoke-GameSync | Show-GameBoardRawGrid
 }
 
+# You can pipe current gameboard string into another function's input 
+Invoke-GameSync | Get-GameBoardCharArray
 
+# You can store current gameboard string into variable and specify it within another function's parameter later
+$GameString = Invoke-GameSync
+Show-GameBoardCharArray -GameBoardRawString $GameString
 
+# To start analyze gameboard
+$GameString = Invoke-GameSync 
+$GameBoard = Get-GameBoardElementArray -GameBoardRawString $GameString
 
-
-
-
-
-
-$AllWalls = Get-GameElementCollection -GameBoardRawString $myBoardString -Element Wall
+# To start analyze gameboard's elements
+$GameString = Invoke-GameSync
+$AllWalls = Get-GameElementCollection -GameBoardRawString $GameString -Element Wall
 $AllWalls.Count
 
-$badGuys = Get-GameElementCollection -GameBoardRawString $myBoardString -Element OtherBomberman
+$badGuys = Get-GameElementCollection -GameBoardRawString $GameString -Element OtherBomberman
 $badGuys.Count
 $badGuys[0]
-$badGuys[1]
-$badGuys[2]
 
-$takeCare = Get-GameElementCollection -GameBoardRawString $myBoardString -Element OtherBombBomberman
-$takeCare[0]
+$beware = Get-GameElementCollection -GameBoardRawString $GameString -Element OtherBombBomberman
+$beware[0]
+
+
+
+
+
+# A kind of algorithm of constatnly playing AI. Place a bomb then move to any free space. 
+#region START
+$myNextAction = "wait"
+$GameTime = 5
+$LastTimeBombPlaced = 0
+while ($true)
+{
+	$GameTime++
+	"`n NEW GameTime is " + $GameTime + " LastTimeBombPlaced was " + $LastTimeBombPlaced
+	
+	$GameString = Invoke-GameSync -NextAction $myNextAction
+	$GameBoard = Get-GameBoardElementArray -GameBoardRawString $GameString
+	
+	# Get Bomber's position
+	# If bomb just been placed, Bomberman is 'BombBomberman' game element
+	$myBombBomber = Get-GameElementCollection -GameBoardRawString $GameString -Element BombBomberman
+	If ($myBombBomber)
+	{
+		$x = $myBombBomber[0][0]
+		$y = $myBombBomber[0][1]
+	}
+	# In general case Bomberman is 'Bomberman' game element
+	$myBomber = Get-GameElementCollection -GameBoardRawString $GameString -Element Bomberman
+	If ($myBomber)
+	{
+		$x = $myBomber[0][0]
+		$y = $myBomber[0][1]
+	}
+	
+	"Bomber at x=$($x) y=$($y)"
+		
+	
+	# Place a bomb if has not been placed last 5 ticks 
+	If (($LastTimeBombPlaced + 5) -lt $GameTime)
+	{
+		$myNextAction = "act"
+		$LastTimeBombPlaced = $GameTime
+		"Placing BOMB" + " at GameTime " + $GameTime
+		Continue
+	}
+	
+	# Looking for any free space to move
+	if ($GameBoard[($x+1),($y)] -match "Space")
+	{
+		$myNextAction = "right"
+		"Moving RIGHT" + " at GameTime " + $GameTime
+		Continue
+	}
+	if ($GameBoard[($x-1),($y)] -match "Space")
+	{
+		$myNextAction = "left"
+		"Moving LEFT" + " at GameTime " + $GameTime
+		Continue
+	}
+	if ($GameBoard[($x),($y+1)] -match "Space")
+	{
+		$myNextAction = "up"
+		"Moving UP" + " at GameTime " + $GameTime
+		Continue
+	}
+	if ($GameBoard[($x),($y-1)] -match "Space")
+	{
+		$myNextAction = "down"
+		"Moving DOWN" + " at GameTime " + $GameTime
+		Continue
+	}
+
+}
+#endregion END
+
+
+
+
+
+
 
 
 
